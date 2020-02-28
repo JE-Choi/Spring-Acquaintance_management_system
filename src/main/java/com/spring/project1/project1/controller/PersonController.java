ef6c71dde64ae11f77f014e96e453dc002d3306b
최지은 <lielieing@naver.com>
2020-02-28 17:07:14 +0900

    - @PostMappint - @RequestBody - Person 정보를 저장하는 API생성

--- src/main/java/com/spring/project1/project1/controller/PersonController.java
+++ src/main/java/com/spring/project1/project1/controller/PersonController.java
@@ -1,21 +1,34 @@
 package com.spring.project1.project1.controller;
 
 import com.spring.project1.project1.domain.Person;
+import com.spring.project1.project1.repository.PersonRepository;
 import com.spring.project1.project1.service.PersonService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "/api/person")
 @RestController
+@Slf4j
 public class PersonController {
     @Autowired
     PersonService personService;
+    @Autowired
+    PersonRepository personRepository;
 
 //    @RequestMapping(method = RequestMethod.GET) "★★★또는★★★"
-    @GetMapping
+    @GetMapping("/{id}")
     // url의 특정 위치에 있는 값을 PathVariable로 받겠다는 의미.
-    @RequestMapping(value = "/{id}")
+//    @RequestMapping(value = "/{id}")
     public Person getPerson(@PathVariable Long id){
         return personService.getPerson(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void postPerson(@RequestBody Person person){ // get이 아니라 requestBody에 넣어서 post로 보낸다는 의미
+        personService.put(person);
+        log.info("person -> {}" , personRepository.findAll());
     }
 }
--- src/main/java/com/spring/project1/project1/domain/Person.java
+++ src/main/java/com/spring/project1/project1/domain/Person.java
@@ -5,6 +5,8 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @Entity
@@ -17,11 +19,18 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Id 자동생성을 위해
     private Long id;
     @NonNull
+    @NotEmpty
+    @Column(nullable = false)
     private String name;
+
     @NonNull
+    @Min(1)
     private int age;
+
     private  String hobby;
     @NonNull
+    @Column(nullable = false)
+    @NotEmpty
     private String bloodType;
     private String address;
     @Valid
